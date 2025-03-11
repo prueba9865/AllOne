d38b785fdef97e243f197f03c6f9d18caca44fc7
prueba9865 <albertoucendo88@hotmail.com>
2025-03-11 18:53:15 +0100

    primera clase creada 'Usuario'

--- src/main/java/com/example/allone/models/Usuario.java
+++ src/main/java/com/example/allone/models/Usuario.java
@@ -1,4 +1,20 @@
 package com.example.allone.models;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class Usuario {
+    private Long id;
+    private String nombre;
+    private String email;
+    private String password;
+    private String avatar;
+    private LocalDateTime created_at;
 }
