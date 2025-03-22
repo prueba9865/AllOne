5f2d7be7da71eaf42e17a87ce947a1ba4efeacc1
prueba9865 <albertoucendo88@hotmail.com>
2025-03-23 00:41:29 +0100

    add avatar upload

--- src/main/java/com/example/allone/controllers/UsuarioController.java
+++ src/main/java/com/example/allone/controllers/UsuarioController.java
@@ -7,6 +7,7 @@ import com.example.allone.config.JwtTokenProvider;
 import com.example.allone.models.Usuario;
 import com.example.allone.repositories.UsuarioRepository;
 import jakarta.validation.Valid;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +18,20 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 @RestController
 public class UsuarioController {
+    private static final List<String> PERMITTED_TYPES = List.of("image/jpeg", "image/png", "image/webp");
+    private static final long MAX_FILE_SIZE = 10000000;
+    private static final String UPLOADS_DIRECTORY = "uploads/avatars";
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -36,12 +46,15 @@ public class UsuarioController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/api/v1/auth/register")
-    public ResponseEntity<Map<String, String>> crearUsuario(@RequestBody @Valid UserRegisterDTO userRegisterDTO) {
+    public ResponseEntity<Map<String, String>> crearUsuario(@RequestBody @Valid UserRegisterDTO userRegisterDTO,
+                                                            @RequestParam("avatar") MultipartFile avatar) {
         if (!userRegisterDTO.getPassword().equals(userRegisterDTO.getPassword2())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                     Map.of("error", "Las contraseñas no coinciden")
             );
         }
+
+        guardarFotos(avatar, userRegisterDTO);
 
         Usuario usuario = this.userRepository.save(
                 Usuario.builder()
@@ -74,5 +87,65 @@ public class UsuarioController {
 
         // Devolvemos un código 200 con el username y token JWT
         return ResponseEntity.ok(new LoginResponseDTO(user.getId(), user.getUsername(), token));
+    }
+
+    public void guardarFotos(MultipartFile foto, UserRegisterDTO userRegisterDTO) {
+        // Directorio temporal donde almacenar las fotos antes de redimensionarlas
+        Path directorioTemporal = Paths.get(System.getProperty("java.io.tmpdir"));
+
+        if (!foto.isEmpty()) {
+            validarArchivo(foto);  // Validar el archivo
+            String nombreFoto = generarNombreUnico(foto);  // Generar un nombre único
+
+            // Rutas del archivo temporal y el archivo final
+            Path rutaTemporal = directorioTemporal.resolve(nombreFoto);
+            Path rutaFinal = Paths.get(UPLOADS_DIRECTORY, nombreFoto);
+
+            try {
+                // Guardar el archivo original en el directorio temporal
+                foto.transferTo(rutaTemporal.toFile());
+
+                // Redimensionar la imagen y guardarla en la ubicación final
+                redimensionarImagen(rutaTemporal.toFile(), rutaFinal.toFile(), 1000);
+
+                // Eliminar el archivo temporal después de redimensionarlo
+                Files.deleteIfExists(rutaTemporal);
+
+                userRegisterDTO.setAvatar(nombreFoto);
+
+            } catch (IOException e) {
+                throw new RuntimeException("Error al guardar y redimensionar la imagen: " + e.getMessage(), e);
+            }
+        }
+    }
+
+    public static void validarArchivo(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new IllegalArgumentException("Archivo no seleccionado");
+        }
+        if (!PERMITTED_TYPES.contains(file.getContentType())) {
+            throw new IllegalArgumentException("El archivo seleccionado no es una imagen.");
+        }
+        if (file.getSize() > MAX_FILE_SIZE) {
+            throw new IllegalArgumentException("Archivo demasiado grande. Sólo se admiten archivos < 10MB");
+        }
+    }
+
+    public static String generarNombreUnico(MultipartFile file) {
+        UUID nombreUnico = UUID.randomUUID();
+        String extension;
+        if (file.getOriginalFilename() != null && !file.getOriginalFilename().isEmpty()) {
+            extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        } else {
+            throw new IllegalArgumentException("El archivo seleccionado no es una imagen.");
+        }
+        return nombreUnico + extension;
+    }
+
+    public void redimensionarImagen(File rutaOriginal, File rutaRedimensionada, int ancho) throws IOException {
+        Thumbnails.of(rutaOriginal)
+                .width(ancho)
+                .keepAspectRatio(true)
+                .toFile(rutaRedimensionada);
     }
 }
